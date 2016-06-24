d1350716018327f489b262a5a550d380538ca763
Matt Payne <Payne@MattPayne.org>
2016-06-24 18:21:00 -0500

    added has_many through statements

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true 
+
+  has_many :authorbooks
+  has_many :books, through: :authorbooks
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ActiveRecord::Base
+  validates :name, presence: true
+
+  has_many :authorbooks
+  has_many :authors, through: :authorbooks
 end
